6ef08dec6ed8aec60d4b6ea93e1bb068c7124ecf
feel2450 <74747288+feel2450@users.noreply.github.com>
2021-05-03 11:14:33 +0900

    Add files via upload

--- address.py
+++ address.py
@@ -1,17 +1,20 @@
 import pickle
-# 202025132 이하민
+# 202025132 이하민 - 2차수정
 
 def main():
    address_book ={} 				# 공백 딕셔너리를 생성한다.
    while True:
        user = display_menu();
        info = []
-       if user == 1:
+       if user == 1: # 추가와 동시에 저장까지 되면서 불러와지게끔
           name, number, addr = get_contact()
           info.append(number)
           info.append(addr)
           address_book[name] = info
           print(address_book)
+          f = open("./addressData.bin", "wb")
+          pickle.dump(address_book, f)
+          f.close()
        elif user == 2:                  # 삭제
           name = get_contact_name()
           address_book.pop(name)		# name을 키로 가지고 항목을 삭제한다.
@@ -26,12 +29,8 @@ def main():
            for key in sorted(address_book):
                print(key, "의 전화번호:", address_book[key][0])
                print(key, "의 주소:", address_book[key][1])
-       elif user == 5:  # 파일 저장하기
-           f = open("./addressData.bin", "wb")
-           pickle.dump(address_book, f)
-           f.close()
-           print("파일이 저장되었습니다!")
-       elif user == 6:  # 파일 불러오기
+
+       elif user == 5:  # 파일 불러오기
            with open("./addressData.bin", "rb") as f:
                #f = open("./addressData.bin", "rb")
                address_book = pickle.load(f)
@@ -56,9 +55,8 @@ def display_menu() :
    print("2. 연락처 삭제")
    print("3. 연락처 검색")
    print("4. 연락처 출력")
-   print("5. 연락처 파일 저장")
-   print("6. 파일 불러 오기")
-   print("7. 종료")
+   print("5. 파일 불러 오기")
+   print("6. 종료")
    select = int(input("메뉴 항목을 선택하시오: "))
    return select
 
